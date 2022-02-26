b832a574eb34fe6267e651329a8ebf4f6e6f511c
L4szl0T0r0k <l4szl0.t0r0k99@gmail.com>
2022-02-26 06:37:23 +0100

    new swipper

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,16 +46,42 @@ function scrollActive(){
     })
 }
 
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        auto:true,
-        loop:true,
-        speed:2000,
-        pauseOnHover: true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        }
+/* === Services modal === */
 
-    });  
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+/* === Swipper === */
+
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
   });
